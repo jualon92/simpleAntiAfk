877c3c3954c530d4920da53919364b9fb1a321af
Juan Ignacio Londra <londrajuan@gmail.com>
2024-06-04 01:59:51 -0300

    chore: Update dependencies and add press key functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,80 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::State;
+ 
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration; 
+use enigo::{
+    Direction::{Press},
+    Enigo, Key, Keyboard, Settings
+};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+
+
+/* #[tauri::command]
+fn start_mouse_click(interval_secs: u64, state: State<'_, AppState>) {
+    let running = state.running.clone();
+    let interval = state.interval.clone();
+
+    *interval.lock().unwrap() = interval_secs;
+    *running.lock().unwrap() = true;
+
+    thread::spawn(move || {
+        let mut enigo = Enigo::new(&Settings::default()).unwrap();
+        loop {
+            {
+                let running = running.lock().unwrap();
+                if !*running {
+                    break;
+                }
+            }
+            thread::sleep(Duration::from_secs(interval_secs));
+            enigo.button(Button::Left, Press).unwrap();
+        }
+    });
+} */
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn start_typing(interval_secs: u64, state: State<'_, AppState>) {
+    let running = state.running.clone();
+    let interval = state.interval.clone();
+
+    *interval.lock().unwrap() = interval_secs;
+    *running.lock().unwrap() = true;
+
+    thread::spawn(move || {
+        let mut enigo = Enigo::new(&Settings::default()).unwrap();
+        loop {
+            {
+                let running = running.lock().unwrap();
+                if !*running {
+                    break;
+                }
+            }
+            thread::sleep(Duration::from_secs(interval_secs));
+            enigo.key(Key::F24, Press).unwrap();
+           
+        }
+    });
+}
+#[tauri::command]
+fn stop_mouse_click(state: State<'_, AppState>) {
+    let mut running = state.running.lock().unwrap();
+    *running = false;
+}
+
+struct AppState {
+    running: Arc<Mutex<bool>>,
+    interval: Arc<Mutex<u64>>,
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(AppState {
+            running: Arc::new(Mutex::new(false)),
+            interval: Arc::new(Mutex::new(60)),
+        })
+        .invoke_handler(tauri::generate_handler![stop_mouse_click, start_typing])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

8de4f360756e8d8b47b5d92082dcc31cb4a60a5e
Juan Ignacio Londra <londrajuan@gmail.com>
2024-06-04 02:17:54 -0300

    clean logs

--- src/actions.ts
+++ src/actions.ts
@@ -4,7 +4,6 @@ import { invoke } from "@tauri-apps/api";
 const TIME_INTERVAL = 60 * 5; // 5min
 export async function stopClicking() {
   try {
-    console.log("Stop Clicking function called"); // Debugging
     await invoke("stop_mouse_click");
   } catch (e) {
     console.error("Failed to stop clicking:", e);
@@ -13,7 +12,6 @@ export async function stopClicking() {
 
 export async function startTyping() {
   try {
-    console.log("Start Typing function called"); // Debugging
     await invoke("start_typing", { intervalSecs: TIME_INTERVAL });
   } catch (e) {
     console.error("Failed to start typing:", e);

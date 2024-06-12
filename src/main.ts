5a1c28f52b70d2edc5d58b80f3c6bdfa402a3b87
Juan Ignacio Londra <londrajuan@gmail.com>
2024-06-11 22:12:37 -0300

    add status indicator

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const startButton = document.getElementById('play-btn') as HTMLElement;
   const startIcon = document.getElementById('play-icon') as HTMLElement;
+  const statusCircle = document.getElementById('status-circle') as HTMLElement;
   startButton?.addEventListener("click", function () {
     const isStartButtonON =	startIcon.classList.contains("fa-play");
 
     if (isStartButtonON) {
       startIcon.classList.remove("fa-play");
       startIcon.classList.add("fa-pause");
-
+      statusCircle.style.backgroundColor = "#48BB78";
       startTyping();
     }else{
       startIcon.classList.remove("fa-pause");
       startIcon.classList.add("fa-play");
       stopClicking();
+      statusCircle.style.backgroundColor = "#F56565";
     }
   })
 	 

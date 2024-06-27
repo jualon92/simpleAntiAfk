ac1fc7b9c6ecb56e98497f16acf314b1440937f6
Juan Ignacio Londra <londrajuan@gmail.com>
2024-06-27 07:18:56 -0300

    tell frontend to manage hiding job

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
- 
+
 use tauri::State;
-use std::sync::{Arc, Mutex};
+use std::sync::{ Arc, Mutex };
 use std::thread;
-use std::time::Duration; 
-use enigo::{
-    Direction::{Press},
-    Enigo, Key, Keyboard, Settings
-};
+use std::time::Duration;
+use enigo::{ Direction::{ Press }, Enigo, Key, Keyboard, Settings };
 use tauri::SystemTray;
 use tauri::AppHandle;
-use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem,  SystemTrayEvent};
+use tauri::{ CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent };
 use tauri::Manager;
 #[tauri::command]
 fn start_typing(interval_secs: u64, state: State<'_, AppState>) {
@@ -22,8 +19,10 @@ fn start_typing(interval_secs: u64, state: State<'_, AppState>) {
     *running.lock().unwrap() = true;
 
     let app_handle = &state.app_handle;
-    app_handle.tray_handle().set_icon(tauri::Icon::Raw(include_bytes!("../icons/power-on.png").to_vec())).unwrap();
-     
+    app_handle
+        .tray_handle()
+        .set_icon(tauri::Icon::Raw(include_bytes!("../icons/power-on.png").to_vec()))
+        .unwrap();
 
     thread::spawn(move || {
         let mut enigo = Enigo::new(&Settings::default()).unwrap();
@@ -36,7 +35,6 @@ fn start_typing(interval_secs: u64, state: State<'_, AppState>) {
             }
             thread::sleep(Duration::from_secs(interval_secs));
             enigo.key(Key::F24, Press).unwrap();
-           
         }
     });
 }
@@ -45,8 +43,10 @@ fn stop_mouse_click(state: State<'_, AppState>) {
     let mut running = state.running.lock().unwrap();
     *running = false;
     let app_handle = &state.app_handle;
-    app_handle.tray_handle().set_icon(tauri::Icon::Raw(include_bytes!("../icons/power-off.png").to_vec())).unwrap();
-     
+    app_handle
+        .tray_handle()
+        .set_icon(tauri::Icon::Raw(include_bytes!("../icons/power-off.png").to_vec()))
+        .unwrap();
 }
 
 #[tauri::command]
@@ -54,9 +54,7 @@ fn hide_app(state: State<'_, AppState>) {
     let app_handle = &state.app_handle;
     let window = app_handle.get_window("main").unwrap();
     window.hide().unwrap();
-     
 }
-
 
 struct AppState {
     running: Arc<Mutex<bool>>,
@@ -67,17 +65,16 @@ struct AppState {
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-    let show = CustomMenuItem::new("show".to_string(), "Show");
+    /*  let show = CustomMenuItem::new("show".to_string(), "Show"); */
     let tray_menu = SystemTrayMenu::new()
-      .add_item(quit)
-      .add_native_item(SystemTrayMenuItem::Separator)
-      .add_item(hide)
-      .add_item(show);
- 
+        .add_item(hide)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+
     let tray = SystemTray::new().with_menu(tray_menu).with_tooltip("anti-afk");
 
-   
-    tauri::Builder::default()
+    tauri::Builder
+        ::default()
         .setup(|app| {
             let app_handle = app.app_handle(); // Obtener AppHandle
             app.manage(AppState {
@@ -88,46 +85,55 @@ fn main() {
             Ok(())
         })
         .system_tray(tray)
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::LeftClick {
-              position: _,
-              size: _,
-              ..
-            } => {
-              println!("system tray received a left click");
-            }
-            SystemTrayEvent::RightClick {
-              position: _,
-              size: _,
-              ..
-            } => {
-              println!("system tray received a right click");
-            }
-            SystemTrayEvent::DoubleClick {
-              position: _,
-              size: _,
-              ..
-            } => {
-              println!("system tray received a double click");
-            }
-            SystemTrayEvent::MenuItemClick { id, .. } => {
-              match id.as_str() {
-                "quit" => {
-                  std::process::exit(0);
+        .on_system_tray_event(|app, event| {
+            match event {
+                SystemTrayEvent::LeftClick { position: _, size: _, .. } => {
+                    println!("system tray received a left click");
                 }
-                "hide" => {
-                  let window = app.get_window("main").unwrap();
-                  window.hide().unwrap();
+                SystemTrayEvent::RightClick { position: _, size: _, .. } => {
+                    println!("system tray received a right click");
                 }
-                "show" => {
-                    let window = app.get_window("main").unwrap();
-                    window.show().unwrap();
-                  }
+                SystemTrayEvent::DoubleClick { position: _, size: _, .. } => {
+                    println!("system tray received a double click");
+                }
+                SystemTrayEvent::MenuItemClick { id, .. } => {
+                    match id.as_str() {
+                        "quit" => {
+                            std::process::exit(0);
+                        }
+                        "hide" => {
+                            let window = app.get_window("main").unwrap();
+                            // Asegúrate de tener acceso a `window` que es una instancia de `AppWindow`
+                            window.emit("hide-window", ()).expect("Failed to emit event");
+                            app.tray_handle()
+                                .set_menu(
+                                    SystemTrayMenu::new()
+                                        .add_item(CustomMenuItem::new("show".to_string(), "Show"))
+                                        .add_native_item(SystemTrayMenuItem::Separator)
+                                        .add_item(CustomMenuItem::new("quit".to_string(), "Salir"))
+                                )
+                                .unwrap();
+                        }
+                        "show" => {
+                            let window = app.get_window("main").unwrap();
+                            window.show().unwrap();
+                            app.tray_handle()
+                                .set_menu(
+                                    SystemTrayMenu::new()
+                                        .add_item(
+                                            CustomMenuItem::new("hide".to_string(), "Ocultar")
+                                        )
+                                        .add_native_item(SystemTrayMenuItem::Separator)
+                                        .add_item(CustomMenuItem::new("quit".to_string(), "Salir"))
+                                )
+                                .unwrap();
+                        }
+                        _ => {}
+                    }
+                }
                 _ => {}
-              }
             }
-            _ => {}
-          })
+        })
         .invoke_handler(tauri::generate_handler![stop_mouse_click, start_typing, hide_app])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");

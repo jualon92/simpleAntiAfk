94705e46b7995034e746cfcb929d7ed977da1daa
Juan Ignacio Londra <jlondra@frd.utn.edu.ar>
2024-07-09 17:52:59 -0300

    add timer invalid/valid notifications

--- src/timeSettings.ts
+++ src/timeSettings.ts
@@ -1,12 +1,33 @@
+import notie from "notie";
+
 export function handleTimerSettings() {
   //set min time for end time input
   const startTimeInput = document.getElementById(
     "start-time"
   ) as HTMLInputElement;
 
-  
   const endTimeInput = document.getElementById("end-time") as HTMLInputElement;
   startTimeInput.addEventListener("change", function () {
     endTimeInput.min = this.value;
   });
+
+
+  //notifications
+  endTimeInput.addEventListener("change", function () {
+    if (endTimeInput.validity.valid) {
+        notie.alert({
+            type:1,
+            text: "Timer set... 🕰️"
+          })
+    }else{
+        notie.alert({
+            type:3,
+            text: "Invalid time... ⏰"
+          })
+    }
+   
+  })
 }
+
+
+

4deea8bb742a07b359fc42f52e134d638bf7ad05
Juan Ignacio Londra <jlondra@frd.utn.edu.ar>
2024-07-22 19:03:01 -0300

    fix multi page nav build via vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
 
@@ -8,6 +8,14 @@ export default defineConfig(async () => ({
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'offtimer.html'),
+      },
+    }
+  },
   server: {
     port: 1420,
     strictPort: true,
